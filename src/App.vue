2d118714eba03517de005c8630ab557c7818ef96
Bhavik <bhavik.tailor101@gmail.com>
2019-03-25 00:28:07 -0400

    just getting everything in there

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+       <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">{{ Title }}</b-navbar-brand>
+        </b-navbar>
+
+        <DataCard 
+          posttitle="HeeeHeee"
+          message="Wow this will be a fun one to read"
+        />
+  </div>
+</template>
+
+<script>
+import DataCard from './components/DataTab.vue'
+export default {
+  name: 'app',
+  components: {
+    DataCard
+  },
+  data () {
+    return {
+      Title: "Demo Boi"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/DataTab.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <b-card 
+          v-bind:title="{posttitle}"
+        >
+          <b-card-text>
+            {{message}}
+          </b-card-text>
+
+          <b-button href="#" variant="primary">View More</b-button>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DataCard",
+    props: ['posttitle', 'message']
+}
+</script>
+
